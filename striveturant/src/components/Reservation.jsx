f0d52fb13ee734e774eb41baf245ab8f607a3840
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-06-05 13:44:03 +0300

    Reservation component added

---
+++ striveturant/src/components/Reservation.jsx
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import ListGroup from 'react-bootstrap/ListGroup'
+
+
+class Reservations extends Component{
+    state ={
+        reservations: []
+        
+    }
+    
+    componentDidMount= async () =>{ 
+        //perfect place for fetches /async operation
+        console.log('I am in component did match method')
+        try{
+            let response = await fetch("https://striveschool.herokuapp.com/api/reservation")
+            let reservations = await response.json()
+            console.log("reservation are: ", reservations)
+            this.setState({
+                reservations
+            })
+        }catch (err) {
+            console.log("error", err)
+            
+        }
+        
+    }
+    
+    render(){
+        return(
+            <div className="mt-2">
+                <ListGroup>
+                {this.state.reservations.length >0 &&
+                    this.state.reservations.map((reservation, i) =>{
+                        return(
+                            <ListGroup.Item key={i}>
+                                From: {reservation.name},  for {reservation.numberOfPersons}
+                                at {reservation.dateTime}
+                            
+                            </ListGroup.Item>
+                            
+                        )
+                    })
+                }
+                    
+ 
+                </ListGroup>
+                {
+                    this.state.reservations.length ===0 && (<div>No reservations for your resturant</div>)
+                }
+            
+            
+            </div>
+            
+        )
+    }
+}
+export default Reservations
